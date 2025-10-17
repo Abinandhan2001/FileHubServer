c222bbba824a5496a20fa3f17ac04be538665060
Abi <abinandhan952@gmail.com>
2025-10-17 22:29:26 +0530

    Initial commit Added sending file list API Added download and upload API Added server ip broadcaster

---
+++ src/main/java/com/filehub/FileHubServer/FMConstants.java
@@ -0,0 +1,8 @@
+package com.filehub.FileHubServer;
+
+public class FMConstants {
+    public static final String FILE_NOT_FOUND = "File not found";
+    public static final String INTERNAL_SERVER_ERROR = "Internal server error";
+    public static final String FILE_FOUND = "File found";
+    public static final String INITIAL_PATH = "E:\\";
+}
